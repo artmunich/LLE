539f35b880c226838654f13c460d72124feb4aba
artmunich <huaixw@gmail.com>
2015-06-13 23:11:31 +0800

    remove redundant files

--- ThreeGS.m
+++
@@ -1,9 +0,0 @@
-%对m*3矩阵的Gram-Schmidt正交化
-function A = ThreeGS(V)
-v1 = V(:,1);
-v2 = V(:,2);
-v3 = V(:,3);
-a1 = v1;
-a2 = v2-((a1'*v2)/(a1'*a1))*a1;
-a3 = v3-((a1'*v3)/(a1'*a1))*a1-((a2'*v3)/(a2'*a2))*a2;
-A = [a1,a2,a3];
